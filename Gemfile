6088a2783fe13345b7787eebc12f919302eee23a
tmgerhart <49044218+tmgerhart@users.noreply.github.com>
2020-11-21 06:08:59 -0600

    Updated help, added readme from task with paging

--- Gemfile
+++ Gemfile
@@ -1,25 +1,19 @@
-# frozen_string_literal: true
-
 source 'https://rubygems.org'
 
-# git_source(:github) do |repo_name|
+#git_source(:github) do |repo_name|
 #  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
 #  "https://github.com/#{repo_name}.git"
-# end
+#end
 
 # Only required for file upload types (Guardium and Qualys to Kenna Direct), comment out if unneeded:
-# gem 'nokogiri'
+#gem 'nokogiri'
 
-gem 'aws-sdk-guardduty'
-gem 'aws-sdk-inspector'
-gem 'json'
 gem 'rest-client'
+gem 'json'
+gem 'aws-sdk-inspector'
+gem 'aws-sdk-guardduty'
+gem 'tty-pager'
 
-group :development, :test do
-  gem 'rubocop', require: false
-end
-
-group :development, :test do
-  gem 'pry'
-  gem 'rspec'
-end
+# TESTING
+gem 'pry'
+gem 'rspec'
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,6 +1,7 @@
 module Kenna
   module Toolkit
     module Helpers
+
       def print_usage
         puts "[ ]                                                                    "
         puts "[+] ========================================================           "
@@ -21,12 +22,15 @@ module Kenna
         puts "[ ]                                                                    "
         puts "[ ] Example:                                                           "
         puts "[ ] task=example option1=true option2=abc                              "
+        puts "[ ] (For help & the readme respectively - if available)                "
+        puts "[ ] task=example help                                                  "
+        puts "[ ] task=example readme                                                "
         puts "[ ]                                                                    "
         puts "[ ] At this time, toolkit usage is strictly UNSUPPORTED.               "
         puts "[ ]                                                                    "
         puts "[ ]                                                                    "
         puts "[ ] Tasks:"
-        TaskManager.tasks.sort_by { |x| x.metadata[:id] }.each do |t|
+        TaskManager.tasks.sort_by{|x| x.metadata[:id] }.each do |t|
           task = t.new
           puts "[+]  - \033[1m#{task.class.metadata[:id]}\033[0m: #{task.class.metadata[:description]}"
         end
@@ -41,27 +45,43 @@ module Kenna
         DateTime.now.strftime("%Y%m%d%H%M%S")
       end
 
-      def print(message = nil)
+      def print(message=nil)
         puts "[ ] (#{timestamp_long}) #{message}"
       end
 
-      def print_good(message = nil)
+      def print_good(message=nil)
         puts "[+] (#{timestamp_long}) #{message}"
       end
 
-      def print_error(message = nil)
+      def print_error(message=nil)
         puts "[!] (#{timestamp_long}) #{message}"
       end
 
-      def print_debug(message = nil)
+      def print_debug(message=nil)
         puts "[D] (#{timestamp_long}) #{message}" if @options && @options[:debug]
       end
 
       def print_task_help(task_name)
-        task = TaskManager.tasks.select { |x| x.metadata[:id] == task_name }.first.new
+        task = TaskManager.tasks.select{|x| x.metadata[:id] == task_name }.first.new
         task.class.metadata[:options].each do |o|
           puts "- Task Option: #{o[:name]} (#{o[:type]}): #{o[:description]}"
           puts "               Required:(#{o[:required]}): Default: #{o[:default]}"
+        end
+      end
+
+      def print_readme(task_name)
+        if(File.exist?("#{$basedir}/tasks/#{task_name}/readme.md"))
+          readme = (File.open("#{$basedir}/tasks/#{task_name}/readme.md")).read
+          readme_header = "\n \n \n \n# ***********************************************\n"
+          readme_header << "#     Displaying readme.md for #{task_name} \n"
+          readme_header << "# ***********************************************\n"
+          readme_header << "\n "
+          readme_header << "\n "
+          readme = readme_header + readme
+          pager = TTY::Pager.new
+          pager.page("#{readme}")
+        else
+          print_error("No readme.md found for #{task_name}")
         end
       end
 
@@ -69,38 +89,40 @@ module Kenna
       ### Helper to read a file consistently
       ###
       def read_input_file(filename)
-        output = File.open(filename, "r").read.gsub!("\r", '')
-        output.sanitize_unicode
+        output = File.open(filename,"r").read.gsub!("\r", '')
+      output.sanitize_unicode
       end
 
       ###
       ### Helper to write a file consistently
       ###
-      def write_file(directory, filename, output)
+      def write_file(directory,filename,output)
+
         FileUtils.mkdir_p directory
 
         # create full output path
         output_path = "#{directory}/#{filename}"
 
         # write it, char by char to avoid large mem issues
-        File.open(output_path, "wb") do |file|
+        File.open(output_path,"wb") do |file|
           output.each_char { |char| file.write char }
         end
       end
 
-      def run_files_on_kenna_connector(connector_id, api_host, api_token, upload_ids, max_retries = 3)
+      def run_files_on_kenna_connector(connector_id, api_host, api_token, upload_ids)
         # optionally upload the file if a connector ID has been specified
-        return unless connector_id && api_host && api_token
+        if connector_id && api_host && api_token
 
-        print_good "Attempting to upload to Kenna API"
-        print_good "Kenna API host: #{api_host}"
+          print_good "Attempting to upload to Kenna API"
+          print_good "Kenna API host: #{api_host}"
 
-        # upload it
-        if connector_id && connector_id != -1
-          kenna = Kenna::Api::Client.new(api_token, api_host)
-          kenna.run_files_on_connector(connector_id, upload_ids, max_retries)
-        else
-          print_error "Invalid Connector ID (#{connector_id}), unable to upload."
+          # upload it
+          if connector_id && connector_id != -1
+            kenna = Kenna::Api::Client.new(api_token, api_host)
+            query_response_json = kenna.run_files_on_connector(connector_id, upload_ids)
+          else
+            print_error "Invalid Connector ID (#{connector_id}), unable to upload."
+          end
         end
       end
 
@@ -108,21 +130,21 @@ module Kenna
       ### Helper to upload to kenna api
       ###
 
-      def upload_file_to_kenna_connector(connector_id, api_host, api_token, filename, run_now = true, max_retries = 3)
+      def upload_file_to_kenna_connector(connector_id, api_host, api_token, filename, run_now=true)
         # optionally upload the file if a connector ID has been specified
-        return unless connector_id && api_host && api_token
+        if connector_id && api_host && api_token
 
-        print_good "Attempting to upload to Kenna API"
-        print_good "Kenna API host: #{api_host}"
+          print_good "Attempting to upload to Kenna API"
+          print_good "Kenna API host: #{api_host}"
 
-        # upload it
-        if connector_id && connector_id != -1
-          kenna = Kenna::Api::Client.new(api_token, api_host)
-          query_response_json = kenna.upload_to_connector(connector_id, filename, run_now, max_retries)
-        else
-          print_error "Invalid Connector ID (#{connector_id}), unable to upload."
+          # upload it
+          if connector_id && connector_id != -1
+            kenna = Kenna::Api::Client.new(api_token, api_host)
+            query_response_json = kenna.upload_to_connector(connector_id, filename, run_now)
+          else
+            print_error "Invalid Connector ID (#{connector_id}), unable to upload."
+          end
         end
-
         return query_response_json
       end
     end
