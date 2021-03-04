de09b269b6bd704437ad05e1f34d2ddfd449c3e3
tmgerhart <49044218+tmgerhart@users.noreply.github.com>
2021-03-03 20:29:30 -0600

    damn rubocop
    again

--- tasks/csv2kdi/csv2kdi.rb
+++ tasks/csv2kdi/csv2kdi.rb
@@ -348,7 +348,6 @@ module Kenna
         filename = "kdiout#{@kenna_connector_id}_#{kdi_subfiles_out += 1}_#{Time.now.strftime('%Y%m%d%H%M%S')}.json"
         kdi_upload @output_dir, filename, @kenna_connector_id, @kenna_api_host, @kenna_api_key, false, @max_retries
         run_files_on_kenna_connector @kenna_connector_id, @kenna_api_host, @kenna_api_key, @uploaded_files
-
       end
     end
   end
