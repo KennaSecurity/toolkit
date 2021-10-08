a6a56c9da57aee4650745b195ad18533ed8fb985
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2021-10-08 10:17:54 -0500

    Placeholder files for Whitehat Sentinel

---
+++ tasks/connectors/whitehat_sentinel/whitehat_sentinel_task.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Kenna
+  module Toolkit
+    class WhitehatSentinelTask < Kenna::Toolkit::BaseTask
+      def self.metadata
+        {
+          id: "whitehat_sentinel",
+          name: "Whitehat Sentinel",
+          description: "This task connects to the Whitehat Sentinel API and pulls results into the Kenna Platform.",
+          options: []
+        }
+      end
+
+      def run(options) # rubocop:disable Lint/UselessMethodDefinition
+        super
+      end
+    end
+  end
+end
