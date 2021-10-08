108c418e1f48c246e3329a668cd8c263e2f8d7da
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2021-10-08 10:36:19 -0500

    Add an API client for talking to Whitehat

---
+++ tasks/connectors/whitehat_sentinel/lib/api_client.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+module Kenna
+  module Toolkit
+    module WhitehatSentinel
+      class ApiClient
+        BASE_PATH = "https://sentinel.whitehatsec.com/api"
+        ASSET_LIMIT = 10
+
+        attr_reader :api_key
+        attr_accessor :logger
+
+        def initialize(api_key:)
+          @api_key = api_key
+        end
+
+        def login
+          get("/")
+        end
+
+        def vulns
+          query = {
+            "display_description" => "custom",
+            "display_default_description" => "1",
+            "display_solution" => "custom",
+            "display_default_solution" => "1",
+            "display_risk" => "1",
+            "display_qanda" => "0",
+            "display_attack_vectors" => "1",
+            "display_attack_vector_notes" => "1",
+            "display_param" => "1",
+            "display_request" => "1",
+            "display_response" => "1",
+            "display_headers" => "1",
+            "display_body" => "1",
+            "display_abbr" => "0"
+          }
+
+          get("/vuln", query)
+        end
+
+        def assets(&block)
+          return to_enum(__method__) unless block_given?
+
+          offset = 0
+          loop do
+            query = {
+              "display_asset" => 1,
+              "display_all" => 1,
+              "page:limit" => ASSET_LIMIT,
+              "page:offset" => offset
+            }
+
+            response = get("/asset", query)
+            parsed = JSON.parse(response, symbolize_names: true)
+            parsed[:collection].each(&block)
+            offset += ASSET_LIMIT
+            break if parsed[:page][:total].to_i <= offset
+          end
+        end
+
+        def sites
+          # We need to know the satellite attribute for all sites, but that data
+          # is only available when requesting an individual site.  Instead of
+          # making a request per-site, we request all sites where satellite is
+          # true and all sites where satellite is false, then combine the results
+          # (while setting the attribute manually).
+          fetch_sites_by_satellite(true).merge(fetch_sites_by_satellite(false))
+        end
+
+        private
+
+        def fetch_sites_by_satellite(satellite)
+          query = {
+            display_entry_points: 1,
+            query_satellite: satellite ? 1 : 0
+          }
+
+          JSON.parse(get("/site", query))
+              .fetch("sites", [])
+              .each_with_object({}) do |site, data|
+            data[site["id"]] = site.slice("label", "entry_points").merge(satellite: satellite)
+          end
+        end
+
+        def get(path, options = {})
+          url = "#{BASE_PATH}#{path}"
+          params = { key: @api_key, format: :json }.merge(options)
+          Kenna::Toolkit::Helpers::Http.http_get(url, { params: params })
+        end
+      end
+    end
+  end
+end
--- tasks/connectors/whitehat_sentinel/whitehat_sentinel_task.rb
+++ tasks/connectors/whitehat_sentinel/whitehat_sentinel_task.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "lib/api_client"
+
 module Kenna
   module Toolkit
     class WhitehatSentinelTask < Kenna::Toolkit::BaseTask
