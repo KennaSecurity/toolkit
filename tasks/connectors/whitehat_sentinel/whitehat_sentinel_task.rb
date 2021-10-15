5907c156a907841f3ecc63aa08b763d565ea9270
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2021-10-15 10:46:41 -0500

    Start constructing assets and findings from the Whitehat API response

--- tasks/connectors/whitehat_sentinel/whitehat_sentinel_task.rb
+++ tasks/connectors/whitehat_sentinel/whitehat_sentinel_task.rb
@@ -27,11 +27,10 @@ module Kenna
         super
 
         # Process:
-        # 1. Retrieve sites from API
-        # 2. Retrieve vulns from API
-        # 3. Retrieve tags from API
-        # 4. Group vulns by URL
-        # 5. Generate KDI doc from vulns
+        # 1. Retrieve findings from API
+        # 2. Retrieve tags from API
+        # 3. Group findings by canonical URL
+        # 4. Generate KDI doc from findings
 
         key = @options[:whitehat_api_key]
         client = Kenna::Toolkit::WhitehatSentinel::ApiClient.new(api_key: key)
@@ -41,11 +40,35 @@ module Kenna
           exit
         end
 
-        sites = client.sites
-        vulns = client.vulns
+        findings = client.vulns
         tag_hash = client.assets.map { |node| node[:asset] }.map { |asset| [asset[:id], tags_for(asset)] }.to_h
 
-        [sites, vulns, tag_hash]
+        findings.group_by { |node| sanitize(node[:url]) }.each do |url, nodes|
+          site_id = nodes.first[:site].to_i
+          asset = {
+            application: nodes.first[:site_name],
+            url: url,
+            tags: tag_hash[site_id]
+          }
+
+          nodes.each do |node|
+            closed_at = Time.parse(node[:closed]) if node[:closed]
+
+            finding = {
+              scanner_identifier: node[:id],
+              scanner_type: "Whitehat Sentinel",
+              severity: node[:severity].to_i * 2,
+              created_at: Time.parse(node[:found]),
+              last_seen_at: closed_at || Time.now,
+              last_fixed_on: closed_at,
+              closed_at: closed_at,
+              vuln_def_name: node[:class],
+              triage_state: map_status_to_triage_state(node[:status])
+            }.compact
+
+            create_kdi_asset_finding(asset, finding)
+          end
+        end
       rescue Kenna::Toolkit::WhitehatSentinel::ApiClient::Error
         print_error "Problem connecting to Whitehat API, please verify the API key."
         exit
@@ -70,6 +93,21 @@ module Kenna
 
       def sanitizer
         @sanitizer ||= Sanitize.new({ remove_contents: false, parser_options: { max_attributes: -1 } })
+      end
+
+      def map_status_to_triage_state(status)
+        case status.upcase
+        when "OPEN"
+          "in_progress"
+        when "CLOSED"
+          "resolved"
+        when "ACCEPTED"
+          "risk_accepted"
+        when "INVALID"
+          "not_a_security_issue"
+        else
+          "new"
+        end
       end
     end
   end
