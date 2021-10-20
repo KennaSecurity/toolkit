f799ebb8356f4300ca9bc2e89976b5194c5e7c87
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2021-10-20 16:03:26 -0500

    Convert to RSpec's one-liner syntax

--- spec/tasks/connectors/whitehat_sentinel/lib/mapper_spec.rb
+++ spec/tasks/connectors/whitehat_sentinel/lib/mapper_spec.rb
@@ -12,25 +12,14 @@ RSpec.describe Kenna::Toolkit::WhitehatSentinel::Mapper do
     let(:closed) { nil }
     let(:node_class) { "Insufficient Transport Layer Protection" }
 
-    it "uses the node's id attribute as scanner_identifier" do
-      expect(mapper.finding_hash(node)).to include(scanner_identifier: node_id)
-    end
+    subject(:finding_hash) { mapper.finding_hash(node) }
 
-    it "sets the scanner_type to Whitehat Sentinel" do
-      expect(mapper.finding_hash(node)).to include(scanner_type: "Whitehat Sentinel")
-    end
-
-    it "sets created_at to the parsed value of the found attribute" do
-      expect(mapper.finding_hash(node)).to include(created_at: found)
-    end
-
-    it "sets last_fixed_on to nil" do
-      expect(mapper.finding_hash(node)).to include(last_fixed_on: nil)
-    end
-
-    it "sets closed_at to nil" do
-      expect(mapper.finding_hash(node)).to include(closed_at: nil)
-    end
+    it { is_expected.to include(scanner_identifier: node_id) }
+    it { is_expected.to include(scanner_type: "Whitehat Sentinel") }
+    it { is_expected.to include(created_at: found) }
+    it { is_expected.to include(last_fixed_on: nil) }
+    it { is_expected.to include(closed_at: nil) }
+    it { is_expected.to include(vuln_def_name: node_class) }
 
     it "sets last_seen_at to now" do
       now = Time.new(2021, 10, 30, 8, 9, 10).utc
@@ -40,24 +29,12 @@ RSpec.describe Kenna::Toolkit::WhitehatSentinel::Mapper do
       end
     end
 
-    it "uses the vuln's class as the vuln_def_name field" do
-      expect(mapper.finding_hash(node)).to include(vuln_def_name: node_class)
-    end
-
     context "when the node has a closed attribute" do
       let(:closed) { Time.new(2021, 10, 23, 11, 12, 13).utc }
 
-      it "sets last_seen_at to the parsed value of the closed attribute" do
-        expect(mapper.finding_hash(node)).to include(last_seen_at: closed)
-      end
-
-      it "sets last_fixed_on to the parsed value of the closed attribute" do
-        expect(mapper.finding_hash(node)).to include(last_fixed_on: closed)
-      end
-
-      it "sets closed_at to the parsed value of the closed attribute" do
-        expect(mapper.finding_hash(node)).to include(closed_at: closed)
-      end
+      it { is_expected.to include(last_seen_at: closed) }
+      it { is_expected.to include(last_fixed_on: closed) }
+      it { is_expected.to include(closed_at: closed) }
     end
   end
 end
