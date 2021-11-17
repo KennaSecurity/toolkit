744ba44c86aeb2ee074f1d202bc7c0a7b44ee19b
indermishra <imishra@securview.com>
2021-11-17 07:28:58 -0800

    Added github dependabot connector

---
+++ tasks/connectors/github_dependabot/github_dependabot.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+require "graphql/client"
+require "graphql/client/http"
+module Kenna
+  module Toolkit
+    class GithubDependabot < Kenna::Toolkit::BaseTask
+
+      def self.metadata
+        {
+          id: "github_dependabot",
+          name: "github_dependabot Vulnerabilities",
+          description: "Pulls assets and vulnerabilitiies from github_dependabot",
+          options: [
+            { name: "github_access_token",
+              type: "api_key",
+              required: true,
+              default: nil,
+              description: "Github Access Token" },
+            { name: "kenna_api_key",
+              type: "api_key",
+              required: false,
+              default: nil,
+              description: "Kenna API Key" },
+            { name: "kenna_api_host",
+              type: "hostname",
+              required: false,
+              default: "api.kennasecurity.com",
+              description: "Kenna API Hostname" },
+            { name: "kenna_connector_id",
+              type: "integer",
+              required: false,
+              default: nil,
+              description: "If set, we'll try to upload to this connector" },
+            { name: "output_directory",
+              type: "filename",
+              required: false,
+              default: "output/github_dependabot",
+              description: "If set, will write a file upon completion. Path is relative to #{$basedir}" }
+          ]
+        }
+      end
+
+      def self.github_graphql_client
+        http = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
+          def headers(context)
+            { "Authorization": "Bearer #{GITHUB_DEPENDABOT_TOKEN}" }
+          end
+        end
+
+        schema = GraphQL::Client.load_schema(http)
+
+        GraphQL::Client.new(schema: schema, execute: http)
+      end
+
+      SECURITY_ADVISORY_QUERY = github_graphql_client.parse <<-'GRAPHQL'
+        query {
+          organization(login: "KennaSecurity") {
+            repositories(orderBy: {field: UPDATED_AT, direction: DESC}, first: 50) {
+              nodes {
+              name
+                vulnerabilityAlerts(last: 50) {
+                  nodes {
+                    id
+                    securityAdvisory {
+                      description
+                      cvss {
+                        score
+                      }
+                      severity
+                      identifiers {
+                        type
+                        value
+                      }
+                      summary
+                      vulnerabilities(last: 50) {
+                        nodes {
+                          package {
+                            name
+                          }
+                          severity
+                          firstPatchedVersion {
+                            identifier
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      def self.security_advisories
+        github_graphql_client.query(SECURITY_ADVISORY_QUERY)
+      end
+
+      def run(opts)
+        super
+
+        @github_access_token = @options[:github_access_token]
+        @kenna_api_host = @options[:kenna_api_host]
+        @kenna_api_key = @options[:kenna_api_key]
+        @kenna_connector_id = @options[:kenna_connector_id]
+
+        repos = GithubDependabot.security_advisories.original_hash["data"]["organization"]["repositories"]["nodes"]
+
+        repo_map = repos.each_with_object({}) do |repo, h|
+          advisories = repo["vulnerabilityAlerts"]["nodes"].map{|alert| alert["securityAdvisory"]}
+          h[repo["name"]] = advisories.reject{|ad| ad["identifiers"].last.has_value?("GHSA") }
+        end
+
+        kdi_hash = {
+          "skip_autoclose": false,
+          "version": 2,
+          "assets":
+            repo_map.map do |repo|
+              {
+                "application": repo.first,
+                "tags":[
+                  "github_dependabot_kdi"
+                ],
+                "vulns": vulnerability_alerts_for(repo)
+              }
+            end,
+          "vuln_defs": vulnerability_definitions(repo_map)
+        }
+
+        # create output dir
+        output_dir = "#{$basedir}/#{@options[:output_directory]}"
+        FileUtils.mkdir_p output_dir
+
+        # create full output path
+        filename = "github_dependabot_kdi.json"
+
+
+        File.open("#{output_dir}/#{filename}","w") do |f|
+          f.write(kdi_hash.to_json)
+        end
+        
+        print_good "Output is available at: #{output_dir}/#{filename}"
+
+        ####
+        ### Finish by uploading if we're all configured
+        ####
+        return unless @kenna_connector_id && @kenna_api_host && @kenna_api_key
+
+        print_good "Attempting to upload to Kenna API at #{@kenna_api_host}"
+        upload_file_to_kenna_connector @kenna_connector_id, @kenna_api_host, @kenna_api_key, "#{output_dir}/#{filename}"
+        
+      end
+
+      def vulnerability_alerts_for(repo)
+        repo.last.map do |alert|
+          {
+             "scanner_identifier": alert["identifiers"].last["value"],
+             "scanner_type": "Github Dependabot",
+             "scanner_score": alert["cvss"]["score"].to_i,
+             "last_seen_at": Time.now.utc,
+             "status": "open",
+             "vuln_def_name": alert["identifiers"].last["value"]
+          }
+        end
+      end
+
+      def vulnerability_definitions(repo_map)
+        all_vulns = repo_map.flat_map do |repo|
+          repo.last
+        end
+
+        all_vulns.map do |advisory|
+          {
+            "scanner_type": "Github Dependabot",
+            "name": advisory["identifiers"].last["value"],
+            "cve_identifiers": advisory["identifiers"].last["value"],
+            "description": advisory["description"]
+          }
+        end
+      end
+
+    end
+  end
+end
---
+++ tasks/connectors/github_dependabot/github_dependabot_readme.md
@@ -0,0 +1,33 @@
+## Running Github Dependabot task 
+
+This toolkit brings in data from github graphql (https://api.github.com/graphql)
+
+To run this task you need the following information from Github account: 
+
+1. Github personal access token
+
+
+Start here to learn how to get personal access token:
+
+>>https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+
+
+
+## Complete list of Options:
+
+| Option | Required | Description | default |
+| --- | --- | --- | --- |
+| github_access_token | true | Github access token | n/a |
+| atp_api_host | false | url to retrieve Defender hosts and vulns | https://api.securitycenter.microsoft.com |
+| atp_oath_host | false | url for Defender authentication | https://login.windows.net |
+| kenna_api_key | false | Kenna API Key for use with connector option | n/a |
+| kenna_api_host | false | Kenna API Hostname if not US shared | api.kennasecurity.com |
+| kenna_connector_id | false | If set, we'll try to upload to this connector | n/a |
+| output_directory | false | If set, will write a file upon completion. Path is relative to #{$basedir} | output/microsoft_atp |
+
+
+## Example Command Line:
+
+For extracting Image vulnerability data:
+
+   toolkit:latest task=github_dependabot github_access_token=v954xxxxxx kenna_connector_id=15xxxx kenna_api_host=api.sandbox.us.kennasecurity.com kenna_api_key=xxx
