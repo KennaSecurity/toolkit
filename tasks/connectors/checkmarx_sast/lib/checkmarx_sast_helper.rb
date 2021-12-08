10ffe234fcb0530f23b0362ea2367c23b27879a4
Fazal Shaik <fshaik@securview.com>
2021-12-08 11:52:43 +0530

    Implemented mapping checkmarxSast data with kenna & upload

--- tasks/connectors/checkmarx_sast/lib/checkmarx_sast_helper.rb
+++ tasks/connectors/checkmarx_sast/lib/checkmarx_sast_helper.rb
@@ -7,7 +7,7 @@ module Kenna
     module CheckmarxSastHelper
       attr_reader :username, :password, :grant_type, :scope, :checkmarx_sast_url, :client_id, :client_secret
 
-      # Method for generating token using username & pwd , client ID and secret
+      # Method for generating token using username & pwd, client ID and secret
       def request_checkmarx_sast_token
         print_debug "Getting Auth Token"
         checkmarx_sast_auth_api_url = "https://#{checkmarx_sast_url}/cxrestapi/auth/identity/connect/token"
@@ -40,11 +40,7 @@ module Kenna
       def fetch_checkmarx_sast_projects(token)
         print_good "Getting Projects \n"
         checkmarx_sast_projects_api_url = "https://#{checkmarx_sast_url}/cxrestapi/projects"
-        headers = {
-          "Content-Type" => "application/json",
-          "accept" => "application/json",
-          "Authorization" => "Bearer #{token}"
-        }
+        headers = bearer_token_headers(token)
         auth_response = http_get(checkmarx_sast_projects_api_url, headers)
         return nil unless auth_response
 
@@ -60,23 +56,58 @@ module Kenna
       def fetch_all_scans_of_project(token, project_id)
         print_good "\n"
         print_good "Getting All Scans of Project ID: #{project_id} \n"
-        checkmarx_sast_scans_api_url = "https://#{checkmarx_sast_url}/cxrestapi/sast/scans?projectId=#{project_id}"
-        headers = {
-          "Content-Type" => "application/json",
-          "accept" => "application/json",
-          "Authorization" => "Bearer #{token}"
-        }
+        checkmarx_sast_scans_api_url = "https://#{checkmarx_sast_url}/cxrestapi/sast/scans?last=1&scanStatus=Finished&projectId=#{project_id}"
+        headers = bearer_token_headers(token)
         auth_response = http_get(checkmarx_sast_scans_api_url, headers)
         return nil unless auth_response
 
         begin
           scan_results = JSON.parse(auth_response.body)
-          print_good "Scan Results: \n"
-          print_good scan_results.to_s
         rescue JSON::ParserError
           print_error "Unable to process scans response!"
         end
         scan_results
+      end
+
+      def generate_report_id_from_scan(token, scan_id)
+        sast_report_generation_api_url = "https://#{checkmarx_sast_url}/cxrestapi/reports/sastScan"
+        headers = bearer_token_headers(token)
+        payload = {
+          ScanId: scan_id,
+          reportType: "XML"
+        }
+        auth_response = http_post(sast_report_generation_api_url, headers, payload)
+        return nil unless auth_response
+
+        begin
+          report = JSON.parse(auth_response.body)
+        rescue JSON::ParserError
+          print_error "Unable to process report generation!"
+        end
+        report["reportId"]
+      end
+
+      def fetch_scan_reports(token, report_id)
+        sast_scan_reports_api_url = "https://#{checkmarx_sast_url}/cxrestapi/reports/sastScan/#{report_id}"
+        headers = bearer_token_headers(token)
+        auth_response = http_get(sast_scan_reports_api_url, headers)
+        return nil unless auth_response
+
+        begin
+          report = Hash.from_xml(auth_response.body)
+        rescue JSON::ParserError
+          print_error "Unable to process scan reports!"
+        end
+        report
+      end
+
+      private
+      def bearer_token_headers(token)
+        {
+          "Content-Type" => "application/json",
+          "accept" => "application/json",
+          "Authorization" => "Bearer #{token}"
+        }
       end
     end
   end
