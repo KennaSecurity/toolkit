ba1bbf8de19fa1e43393fd609caedd638a29dff0
Nick Henry <nicholas.henry@kennasecurity.com>
2023-02-27 13:44:12 -0600

    rubocop lint fix

--- tasks/connectors/wiz/lib/vulns_mapper.rb
+++ tasks/connectors/wiz/lib/vulns_mapper.rb
@@ -100,7 +100,8 @@ module Kenna
         def extract_ec2_instance_id(vuln)
           arn = vuln['vulnerableAsset']['providerUniqueId'] if vuln['vulnerableAsset']['providerUniqueId'].present?
           cloud_platform = vuln['vulnerableAsset']['cloudPlatform'] if vuln['vulnerableAsset']['cloudPlatform'].present?
-          return nil if !defined? arn || !defined? cloud_platform || cloud_platform != 'AWS' || !arn.starts_with('arn:')
+          return nil unless defined? arn && defined? cloud_platform && cloud_platform == 'AWS' && arn.starts_with('arn:')
+
           arn.split('/')[1]
         end
       end
