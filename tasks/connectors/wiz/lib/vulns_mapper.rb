82e7a4153565b0994bdaf7189f3b0ad2601aa3e7
Nick Henry <nicholas.henry@kennasecurity.com>
2023-03-01 10:59:23 -0600

    readding the extraction in extract asset block

--- tasks/connectors/wiz/lib/vulns_mapper.rb
+++ tasks/connectors/wiz/lib/vulns_mapper.rb
@@ -21,6 +21,7 @@ module Kenna
             "hostname" => extract_hostname(vuln),
             "os" => (vuln["vulnerableAsset"]["operatingSystem"] if vuln["vulnerableAsset"]["type"] == "VIRTUAL_MACHINE"),
             "ip_address" => ((vuln["vulnerableAsset"]["ipAddresses"] || []).first if vuln["vulnerableAsset"]["type"] == "VIRTUAL_MACHINE"),
+            "ec2" => extract_ec2_instance_id(vuln),
             "tags" => (extract_tags(vuln) if vuln["vulnerableAsset"]["tags"].present?)
           }.compact
           asset["asset_type"] = "image" if asset["image_id"]
