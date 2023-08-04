2fc80af32b26e501d6e5a8cad90a7eebaa7cc8db
ivoloshy <ivoloshy@cisco.com>
2023-08-04 09:23:53 -0600

    refactor specs

--- spec/tasks/connectors/cylera/cylera_task_spec.rb
+++ spec/tasks/connectors/cylera/cylera_task_spec.rb
@@ -17,11 +17,16 @@ RSpec.describe Kenna::Toolkit::CyleraTask do
       }
     end
     let(:connector_run_success) { true }
-    let(:kenna_client) { instance_double(Kenna::Api::Client, upload_to_connector: { 'data_file' => 12 }, run_files_on_connector: { 'success' => connector_run_success }) }
-
-    before do
-      allow(Kenna::Api::Client).to receive(:new) { kenna_client }
+    let(:kenna_client) do
+      instance_double(
+        Kenna::Api::Client,
+        upload_to_connector: { 'data_file' => 12 },
+        run_files_on_connector: { 'success' => connector_run_success },
+        get_connector_runs: { results: [{ success: true, start_time: Time.now.to_s }] }
+      )
     end
+
+    before { allow(Kenna::Api::Client).to receive(:new) { kenna_client } }
 
     it 'succeeds' do
       expect { task.run(options) }.to_not raise_error
@@ -52,29 +57,12 @@ RSpec.describe Kenna::Toolkit::CyleraTask do
     end
 
     context 'when time value is an unexpected format' do
-      let(:options) do
-        {
-          cylera_api_host: 'cylera.host',
-          cylera_api_user: 'api_user',
-          cylera_api_password: 'api_pass',
-          kenna_api_key: 'api_key',
-          kenna_api_host: 'kenna.example.com',
-          kenna_connector_id: '12',
-          cylera_last_seen_after: '60m'
-        }
-      end
-
       it 'exits the script' do
-        expect { task.run(options) }.to raise_error(RuntimeError)
+        expect { task.run(options.merge(cylera_last_seen_after: '60m')) }.to raise_error(RuntimeError)
       end
     end
 
     context 'when the incremental param present' do
-      before do
-        allow(kenna_client).to receive(:get_connector_runs)
-          .and_return({ results: [{ 'success': true, 'start_time': Time.now.to_s }] })
-      end
-
       it 'calls the connector runs endpoint' do
         expect(kenna_client).to receive(:get_connector_runs)
         expect { task.run(options.merge(incremental: true)) }.to_not raise_error
