81fcc137b952343d94222f4cfea73da7f36aa314
effany <72710849+effany@users.noreply.github.com>
2024-01-17 23:16:04 +0800

    remove trailling whitespace

--- tasks/connectors/qualys_was/qualys_was.rb
+++ tasks/connectors/qualys_was/qualys_was.rb
@@ -230,7 +230,7 @@ module Kenna
       end
 
       def name(find_from)
-        return if find_from.nil? 
+        return if find_from.nil?
         
         structured_name = [find_from['name'], find_from['qid'], find_from['id']].compact.join(' - ')
         structured_name unless structured_name.empty?
