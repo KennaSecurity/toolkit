5a2011bc56d503f1791c716615e1a1d2924bb25e
jesutorrr <121052811+jesutorrr@users.noreply.github.com>
2024-03-09 12:26:40 -0600

    Autorun1
    Changing to $stdin

--- tasks/connectors/whitesource/whitesource_task.rb
+++ tasks/connectors/whitesource/whitesource_task.rb
@@ -72,8 +72,7 @@ module Kenna
         initialize_options
 
         puts "Introduce the API Base URL (v1.4) of your Mend Organization:"
-        api_base_url = STDIN.gets.chomp
-        puts "Working on the task with API Base URL: " + api_base_url
+        api_base_url = $stdin.gets.chomp.to_s
 
         client = Kenna::Toolkit::Whitesource::Client.new(api_base_url, @user_key, @request_type, @request_token, @alert_type, @days_back)
 
