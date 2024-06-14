00f0ec889d48e885b8a3715a7c2f428a27a83542
jesutorrr <121052811+jesutorrr@users.noreply.github.com>
2024-06-14 08:35:24 -0600

    Update snyk_v2_stubs.rb

--- spec/tasks/connectors/snyk_v2/snyk_v2_stubs.rb
+++ spec/tasks/connectors/snyk_v2/snyk_v2_stubs.rb
@@ -1,24 +1,22 @@
-# frozen_string_literal: true
+# snyk_v2_stubs.rb
 
 module SnykV2Stubs
   def read_fixture_file(filename)
-    File.read(File.join(%w[spec tasks connectors snyk_v2 fixtures], filename))
+    File.read(File.join('spec', 'fixtures', 'vcr_cassettes', filename))
   end
 
   def stub_orgs_request
     stub_request(:get, "https://api.snyk.io/rest/orgs?version=2024-04-29")
-      .to_return_json(body: read_fixture_file("orgs.json"))
+      .to_return(status: 200, body: read_fixture_file("orgs.json"), headers: {})
   end
 
   def stub_projects_request(org_id)
     stub_request(:get, "https://api.snyk.io/rest/orgs/#{org_id}/projects?version=2024-04-29&limit=100")
-      .to_return_json(body: read_fixture_file("projects.json"))
+      .to_return(status: 200, body: read_fixture_file("projects.json"), headers: {})
   end
 
   def stub_issues_request(org_id, from_date, to_date)
     stub_request(:get, "https://api.snyk.io/rest/orgs/#{org_id}/issues?version=2024-04-29&limit=100&created_after=#{from_date}&created_before=#{to_date}")
-      .to_return_json(body: read_fixture_file("issues.json"))
-    stub_request(:get, "https://api.snyk.io/rest/orgs/#{org_id}/issues?version=2024-04-29&limit=100&created_after=#{from_date}&created_before=#{to_date}")
-      .to_return_json(body: read_fixture_file("issues_empty.json"))
+      .to_return(status: 200, body: read_fixture_file("issues.json"), headers: {})
   end
 end
