69169b39a387bafc75c184d37e055b5c2dd8adf6
jesutorrr <121052811+jesutorrr@users.noreply.github.com>
2024-07-16 08:37:21 -0600

    Update snyk_v2_task_spec.rb

--- spec/tasks/connectors/snyk_v2/snyk_v2_task_spec.rb
+++ spec/tasks/connectors/snyk_v2/snyk_v2_task_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "rspec_helper"
+require "spec_helper"
 require_relative "snyk_v2_stubs"
 require "json"
 
@@ -8,7 +8,7 @@ RSpec.describe Kenna::Toolkit::SnykV2Task do
   include SnykV2Stubs
   subject(:task) { described_class.new }
 
-  describe "#run" do
+  describe "#run", vcr: { cassette_name: "snyk_v2_task_run" } do
     let(:connector_run_success) { true }
     let(:kenna_client) { instance_double(Kenna::Api::Client, upload_to_connector: { "data_file" => 12 }, run_files_on_connector: { "success" => connector_run_success }) }
     let(:options) do
@@ -31,27 +31,23 @@ RSpec.describe Kenna::Toolkit::SnykV2Task do
     let(:org_id) { JSON.parse(read_fixture_file("orgs.json"))["data"].first["id"] }
 
     before do
-      VCR.use_cassette("snyk_v2_task_run") do
-        stub_orgs_request
-        stub_projects_request(org_id)
-        stub_issues_request(org_id, options[:from_date], options[:to_date])
-        allow(Kenna::Api::Client).to receive(:new).and_return(kenna_client)
-        spy_on_accumulators
-      end
+      stub_orgs_request
+      stub_projects_request(org_id)
+      stub_issues_request(org_id, options[:from_date], options[:to_date])
+      allow(Kenna::Api::Client).to receive(:new).and_return(kenna_client)
+      spy_on_accumulators
     end
 
     it "runs the task and includes the expected vulnerability definition" do
-      VCR.use_cassette("snyk_v2_task_run") do
-        task.run(options)
-        expect(task.vuln_defs).to include(
-          {
-            "description" => "Deserialization of Untrusted Data",
-            "name" => "CVE-2015-7501",
-            "scanner_identifier" => "SNYK-JAVA-COMMONSCOLLECTIONS-30078",
-            "scanner_type" => "Snyk"
-          }
-        )
-      end
+      task.run(options)
+      expect(task.vuln_defs).to include(
+        {
+          "description" => "Deserialization of Untrusted Data",
+          "name" => "CVE-2015-7501",
+          "scanner_identifier" => "SNYK-JAVA-COMMONSCOLLECTIONS-30078",
+          "scanner_type" => "Snyk"
+        }
+      )
     end
   end
 
