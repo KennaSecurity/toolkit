cfa0d3b2adde918946aeb6b8e83302de601b6da0
jesutorrr <121052811+jesutorrr@users.noreply.github.com>
2024-07-26 07:04:18 -0600

    updating VCR

--- spec/tasks/connectors/snyk_v2/snyk_v2_stubs.rb
+++ spec/tasks/connectors/snyk_v2/snyk_v2_stubs.rb
@@ -2,21 +2,20 @@
 
 module SnykV2Stubs
   def read_fixture_file(filename)
-    File.read(File.join('spec', 'tasks', 'connectors', 'snyk_v2', 'fixtures', filename))
+    File.read(File.join(%w[spec tasks connectors snyk_v2 fixtures], filename))
   end
 
   def stub_orgs_request
-    stub_request(:get, "https://api.snyk.io/rest/orgs?version=2024-04-29")
-      .to_return(status: 200, body: read_fixture_file("orgs.json"), headers: {})
+    stub_request(:get, "https://api.snyk.io/rest/orgs?version=2024-04-29").to_return_json(body: read_fixture_file("orgs.json"))
   end
 
-  def stub_projects_request(org_id)
-    stub_request(:get, "https://api.snyk.io/rest/orgs/#{org_id}/projects?version=2024-04-29&limit=100")
-      .to_return(status: 200, body: read_fixture_file("projects.json"), headers: {})
+  def stub_projects_request
+    stub_request(:get, Addressable::Template.new("https://api.snyk.io/rest/orgs/{orgId}/projects?version=2024-04-29&limit=100"))
+      .to_return_json(body: read_fixture_file("projects.json"))
   end
 
-  def stub_issues_request(org_id, from_date, to_date)
-    stub_request(:get, "https://api.snyk.io/rest/orgs/#{org_id}/issues?version=2024-04-29&limit=100&created_after=#{from_date}&created_before=#{to_date}")
-      .to_return(status: 200, body: read_fixture_file("issues.json"), headers: {})
+  def stub_issues_request
+    stub_request(:get, Addressable::Template.new("https://api.snyk.io/rest/orgs/{orgId}/issues?version=2024-04-29&limit=10&created_after=2024-01-01T00:00:00Z&created_before=2024-01-30T00:00:00Z"))
+      .to_return_json(body: read_fixture_file("issues.json"))
   end
 end
--- spec/tasks/connectors/snyk_v2/snyk_v2_task_spec.rb
+++ spec/tasks/connectors/snyk_v2/snyk_v2_task_spec.rb
@@ -1,53 +1,66 @@
 # frozen_string_literal: true
 
-require "spec_helper"
+require "rspec_helper"
 require_relative "snyk_v2_stubs"
-require "json"
 
 RSpec.describe Kenna::Toolkit::SnykV2Task do
   include SnykV2Stubs
   subject(:task) { described_class.new }
 
-  describe "#run", vcr: { cassette_name: "snyk_v2_task_run" } do
+  describe "#run" do
     let(:connector_run_success) { true }
     let(:kenna_client) { instance_double(Kenna::Api::Client, upload_to_connector: { "data_file" => 12 }, run_files_on_connector: { "success" => connector_run_success }) }
-    let(:options) do
-      {
-        snyk_api_token: '0xdeadbeef',
-        from_date: "2024-05-06T00:00:00Z",
-        to_date: "2024-06-05T00:00:00Z",
-        include_license: false,
-        page_size: 100,
-        batch_size: 500,
-        page_num: 5000,
-        kenna_connector_id: nil,
-        kenna_api_key: nil,
-        kenna_api_host: "api.kennasecurity.com",
-        output_directory: "output/snyk",
-        snyk_api_base: "api.snyk.io"
-      }
-    end
-
-    let(:org_id) { JSON.parse(read_fixture_file("orgs.json"))["data"].first["id"] }
+    let(:options) { { snyk_api_token: '0xdeadbeef', import_type: } }
 
     before do
       stub_orgs_request
-      stub_projects_request(org_id)
-      stub_issues_request(org_id, options[:from_date], options[:to_date])
-      allow(Kenna::Api::Client).to receive(:new).and_return(kenna_client)
+      stub_projects_request
+      stub_issues_request
+      allow(Kenna::Api::Client).to receive(:new) { kenna_client }
       spy_on_accumulators
+      task.run(options)
     end
 
-    it "runs the task and includes the expected vulnerability definition" do
-      task.run(options)
-      expect(task.vuln_defs).to include(
-        {
-          "description" => "Deserialization of Untrusted Data",
-          "name" => "CVE-2015-7501",
-          "scanner_identifier" => "SNYK-JAVA-COMMONSCOLLECTIONS-30078",
-          "scanner_type" => "Snyk"
-        }
-      )
+    context "vulnerability" do
+      let(:import_type) { "vulns" }
+
+      it "creates normalized (non-duplicative) vuln_defs" do
+        VCR.use_cassette('snyk_v2_task_run', record: :new_episodes) do
+          task.run(options)
+        end
+
+        expect(task.vuln_defs).to include(
+          {
+            "name" => "SNYK-RHEL9-GLIB2-6820066",
+            "scanner_type" => "Snyk",
+            "cve_identifiers" => "CVE-2024-34397",
+            "cwe_identifiers" => "CWE-940",
+            "description" => "Improper Verification of Source of a Communication Channel",
+            "solution" => "For more information, go to this link: https://nvd.nist.gov/vuln/detail/CVE-2024-34397"
+          }
+        )
+      end
+
+      it "creates normalized (non-duplicative) vulns on assets" do
+        expect(task.assets).to include(
+          {
+            "file" => "Snyk_RHEL9_GLIB2_6820066_f5798261-5db0-442a-9eb8-10bf21d50888",
+            "tags" => ["Org:e0319d01-7a3f-442a-8e94-3613b81c705a"],
+            "os" => "RHEL9",
+            "priority" => 10,
+            "vulns" => [
+              { "scanner_identifier" => "f5798261-5db0-442a-9eb8-10bf21d50888",
+                "vuln_def_name" => "SNYK-RHEL9-GLIB2-6820066",
+                "scanner_type" => "Snyk",
+                "created_at" => "2024-05-11T01:12:50.945Z",
+                "last_seen_at" => "2024-05-11T01:12:51.532107Z",
+                "status" => "open",
+                "details" => "CVE-2024-34397 : Improper Verification of Source of a Communication Channel",
+                "scanner_score" => 4 }
+            ]
+          }
+        )
+      end
     end
   end
 
