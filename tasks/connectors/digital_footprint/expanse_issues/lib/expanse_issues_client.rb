f69a3c300a8732617be54cd5941755e081b2e462
Jason Garber <122489457+jgarber-cisco@users.noreply.github.com>
2025-04-22 09:56:21 -0400

    Revert "Fix SSRF vulnerability"
    This reverts commit bbd41031a6ae52c0b42bbefea2a27b57fbf038c5. CodeQL will continue flagging this until we fix all 12 code paths, which we're not doing. This is client-side code running against a client's own resources.

--- tasks/connectors/digital_footprint/expanse_issues/lib/expanse_issues_client.rb
+++ tasks/connectors/digital_footprint/expanse_issues/lib/expanse_issues_client.rb
@@ -7,8 +7,6 @@ module Kenna
   module Toolkit
     module ExpanseIssues
       class ExpanseIssuesClient
-        BASE_URL = "https://expander.expanse.co/api/v1/issues"
-
         def initialize(api_key)
           url = "https://expander.qadium.com/api/v1/idtoken"
           response = http_get(url, { Authorization: "Bearer #{api_key}" })
@@ -21,14 +19,14 @@ module Kenna
         end
 
         def issue_types
-          url = "#{BASE_URL}/issueTypes?includeArchived=false&sort=id"
+          url = "https://expander.expanse.co/api/v1/issues/issueTypes?includeArchived=false&sort=id"
           response = http_get(url, @headers)
           result = JSON.parse(response.body)
           result["data"].map { |x| x["id"] }
         end
 
         def business_units
-          url = "#{BASE_URL}/businessUnits"
+          url = "https://expander.expanse.co/api/v1/issues/businessUnits"
           response = http_get(url, @headers)
           result = JSON.parse(response.body)
           result["data"].map { |x| x["id"] }
@@ -38,21 +36,28 @@ module Kenna
           return nil unless successfully_authenticated?
 
           out = []
+          # issue_types.lazy.each do |issue_type|
+          # start with sensible defaults
           page = 0
           modified_after = (DateTime.now - lookback.to_i).strftime("%FT%TZ")
-          url = "#{BASE_URL}/issues?&activityStatus=Active&progressStatus=New,Investigating,InProgress&limit=#{limit_per_page}&issueTypeId=#{issue_type}&businessUnit=#{business_unit}&modifiedAfter=#{modified_after}"
-          url += "&priority=#{priorities}" unless priorities.nil?
-          url += "&tagName=#{tags}" unless tags.nil?
+          url = "https://expander.expanse.co/api/v1/issues/issues?&activityStatus=Active&progressStatus=New,Investigating,InProgress&limit=#{limit_per_page}&issueTypeId=#{issue_type}&businessUnit=#{business_unit}&modifiedAfter=#{modified_after}"
+          url = "#{url}&priority=#{priorities}" unless priorities.nil?
+          url = "#{url}&tagName=#{tags}" unless tags.nil?
 
           until url.nil?
+
+            # bump our page up
             page += 1
+            # get the listing
             response = http_get(url, @headers)
             result = JSON.parse(response.body)
 
+            # puts "DEBUG Got #{result["data"].count} cloud exposures"
+
             out.concat(result["data"])
             url = result["pagination"].fetch("next")
-            raise "Potential SSRF detected: URL does not match base URL" unless url&.start_with?(BASE_URL)
           end
+          # end
 
           out
         end
