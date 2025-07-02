5dfa6784409c75d1b796fa8a999148cdece1a005
effany <72710849+effany@users.noreply.github.com>
2025-07-02 12:48:39 +0200

    remove trailing white space

--- lib/faraday_middlewares/faraday_hmac_middleware.rb
+++ lib/faraday_middlewares/faraday_hmac_middleware.rb
@@ -13,14 +13,14 @@ class FaradayHmac < Faraday::Middleware
     auth_path = "#{uri.path}?#{uri.query}"
     auth_header = @client.hmac_auth_options(auth_path)[:Authorization]
     env.request_headers['Authorization'] = auth_header
-    
+
     # Log when HMAC is being generated
     if auth_header&.include?('VERACODE-HMAC-SHA-256')
       timestamp = auth_header[/ts=(\d+)/, 1]
       nonce = auth_header[/nonce=([^,]+)/, 1]
       puts "HMAC MIDDLEWARE: Generated auth for #{env.method.upcase} #{env.url} - ts=#{timestamp}, nonce=#{nonce}"
     end
-    
+
     @app.call(env)
   end
 end
